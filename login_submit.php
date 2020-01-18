752ebb26dafff5e6692e60076f9e40155048b544
Amrit <amritkashu4@gmail.com>
2020-01-18 23:53:16 +0530

    submit.php

---
+++ login_submit.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    $hostname="127.0.0.1";
+    $username="root";
+    $db_password="123456";
+    $db_name="ungineering";
+    
+    $conn = mysqli_connect($hostname,$username,$db_password,$db_name);
+    if(!$conn){
+        die("connection failed: ". mysqli_connect_error());
+    }
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $flag=0;
+    $sql= "SELECT * FROM users";
+    $result = mysqli_query($conn,$sql);
+    if(!$result){
+        die("Error: " . $sql . "<br>" . mysqli_error($conn));
+    }
+    while($row=mysqli_fetch_array($result))
+    {
+        if(($row['email']==$email) && ($row['password']==$password))
+        {
+            $flag=1;
+            break;
+        }
+    }
+    if($flag==0)
+    {
+        echo(" INVALID USERNAME OR PASSWORD");
+    }   
+    else
+    {
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['name'] = $row['name'];
+        ?>
+        <a href="loggedin_homepage.php">CLICK HERE <br/></a> TO GO TO HOME PAGE.
+        <?php        
+    }
+    mysqli_close($conn);  
+?>  
---
+++ register_submit.php
@@ -0,0 +1,21 @@
+<?php
+    $hostname="127.0.0.1";
+    $username="root";
+    $db_password="123456";
+    $db_name="ungineering";
+    
+    $conn = mysqli_connect($hostname,$username,$db_password,$db_name);
+    if(!$conn){
+        die("connection failed: ". mysqli_connect_error());
+    }
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $sql = "INSERT INTO users (name,email,password) VALUES ('$name','$email','$password')";
+    $result = mysqli_query($conn,$sql);
+    if(!$result){
+        die("Error:". $sql . "<br/>" . mysqli_error($conn));
+    }
+    echo "Registration Sucessfull";
+    mysqli_close($conn);  
+?>  
