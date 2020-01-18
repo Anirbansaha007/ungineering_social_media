3deff5a4a3c04c0fbe862bc144e4e1f798dbfc4a
Amrit <amritkashu4@gmail.com>
2020-01-18 16:55:47 +0530

    sql

---
+++ social_media_database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE social_media;
+USE social_media;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    college VARCHAR(255),
+    phone_number VARCHAR(13),
+    PRIMARY KEY (id) 
+);
+CREATE TABLE statuses (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    status VARCHAR(1000),
+    date VARCHAR(255),
+    time VARCHAR(255),
+    PRIMARY KEY (id), 
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
